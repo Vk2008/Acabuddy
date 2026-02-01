66968d665a12d1cb3bb82e66c27500d947593e6e
Vanshika Kataria <72748105+Vk2008@users.noreply.github.com>
2026-02-01 14:53:40 +0530

    Replace ImageField with CloudinaryField in models

--- registration/app1/models.py
+++ registration/app1/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+
 
 class Question(models.Model):
     title = models.CharField(max_length=255)
@@ -9,11 +11,11 @@ class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     tags = models.CharField(max_length = 200, blank = True, help_text = "Comma-separated tags")
     created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(
-        upload_to='questions/',
-        blank=True,
-        null=True
-    )
+    image = CloudinaryField(
+    'image',
+    blank=True,
+    null=True
+)
     def tag_list(self):
         return [t.strip() for t in self.tags.split(',') if t.strip()]
 
@@ -26,12 +28,11 @@ class Answer(models.Model):
     ai_reasoning = models.TextField(blank=True)
     ai_audit = models.TextField(blank=True)
 
-    image = models.ImageField(
-        upload_to='answers/',
-        blank=True,
-        null=True
-    )
-
+    image = CloudinaryField(
+    'image',
+    blank=True,
+    null=True
+)
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
@@ -42,4 +43,5 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
+
         Profile.objects.create(user=instance)
